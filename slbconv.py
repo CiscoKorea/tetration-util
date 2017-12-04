a17be201a2ccf04c656e29793f7ba9dc72224e9b
Charles Hyungsoo Kim <javaos74@gmail.com>
2017-12-05 02:11:34 +0900

    add convert custom slb configuration
    convert csv formated slb configuration to normalized json format

---
+++ slbconv.py
@@ -0,0 +1,48 @@
+#-*- coding:utf-8 -*-
+
+import json 
+import sys, getopt
+
+slb_conf = {}
+slb_conf['configs'] = []
+
+def conv_to_l4_json( src, dst):
+    with open( src, 'r') as inf:
+        for line in inf.readlines():
+            line = line.strip()
+            items = line.split(",")
+            name = items[0]
+            for port in items[2].split():
+                l4_entry = {}
+                l4_entry['name'] = name + '-' + port
+                l4_entry['vip'] = items[1]
+                l4_entry['vip_port'] = port
+                backends = []
+                for bip in items[3].split():
+                    backends.append( {'backend_ip': bip, 'backend_port': port})
+                l4_entry['backends'] = backends
+                slb_conf['configs'].append( l4_entry)
+    with open( dst, 'w+') as outf:
+        outf.write( json.dumps( slb_conf))
+
+
+
+if __name__ == '__main__':
+    # Read command line args
+    infile = None
+    outfile = None
+    myopts, args = getopt.getopt(sys.argv[1:],"i:o:")
+    for o, a in myopts:
+        if o == '-i':
+            infile = a
+        elif o == '-o':
+            outfile = a
+        else:
+            print("Usage : {0} -i input -o output ".format( sys.argv[0]))
+            sys.exit(0)
+    if infile  and outfile:
+        print("input file {0}, and output file {1}".format( infile, outfile))
+        conv_to_l4_json( infile, outfile)
+    else:
+        print("Usage : %s -i input -o output ".format( sys.argv[0]))
+        sys.exit(0)
