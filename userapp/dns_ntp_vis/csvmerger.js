88618e1ef142750b64bbd0702c5c6d5e68796bbe
Charles Hyungsoo Kim <javaos74@gmail.com>
2018-07-13 14:24:36 +0900

    Update csvmerger.js
    add command line parameter  director path and output csv file

--- userapp/dns_ntp_vis/csvmerger.js
+++ userapp/dns_ntp_vis/csvmerger.js
@@ -2,18 +2,20 @@ var fs = require('fs')
 
 var args = process.argv.slice(2)
 var path = args[0]
+var csv_file = args[1]
 fs.readdir( path, (err, files) => {
     if( err) throw Error;
-    fs.writeFileSync( 'merged.cvs', "src_address,dst_address,dst_port,proto,tot_fwd_bytes,tot_rev_bytes\n")
+    fs.writeFileSync( csv_file, "src_address,dst_address,flow_count\n")
     files.forEach( buf => {
         file = buf.toString()
         if( file.indexOf("part-") == 0) {
-            fs.readFile( path + '/' + file, (err, buf) => {
+            fs.readFile( path + '\\' + file, (err, buf) => {
                 lines = buf.toString().split('\n')
-                for( var i =1; i < lines.length; i++)
-                    fs.appendFileSync( 'merged.cvs', lines[i]+'\n')
+                for( var i =1; i < lines.length && lines[i].length > 2; i++)
+                    fs.appendFileSync( csv_file, lines[i]+'\n')
             });
         }
     });
 });
+
 
